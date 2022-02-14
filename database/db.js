8c3c774cd63fb68bacb48f15787ee826eb5e0cb8
mohaperu <mohaperu@gmail.com>
2022-02-14 20:03:07 +0530

    pushed

--- database/db.js
+++ database/db.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 
 
-const Connection = async (username, password) => {
-    const URL = `mongodb+srv://mohan111:mohan111@cluster0.fnbwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const Connection = async () => {
+    const URL = "mongodb+srv://mohan111:mohan111@cluster0.fnbwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
         console.log('Database connected successfully');
--- index.js
+++ index.js
@@ -21,9 +21,7 @@ app.use('/', Router);
 
 
 const PORT = 8000;
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
 
-Connection(username, password);
+Connection();
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
